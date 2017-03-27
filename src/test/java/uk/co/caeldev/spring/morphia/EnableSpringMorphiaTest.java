a0d22108d79bb24610f626ff9327860cc6e23ae2
Prussia <prussia.hu@pwc.com>
2017-03-27 07:48:17 -0400

    format

--- src/test/java/uk/co/caeldev/spring/morphia/EnableSpringMorphiaTest.java
+++ src/test/java/uk/co/caeldev/spring/morphia/EnableSpringMorphiaTest.java
@@ -16,23 +16,28 @@ import static uk.org.fyodor.generators.RDG.string;
 @SpringBootTest
 @ActiveProfiles("test")
 public class EnableSpringMorphiaTest {
-    
-    @Autowired
-    private Datastore datastore;
-    
-    @Test
-    public void shouldPersistEntity() throws Exception {
-        //Given
-        final String name = string().next();
-        final TestEntity testEntity = new TestEntity(null, name);
 
-        //When
-        final Key<TestEntity> save = datastore.save(testEntity);
+	@Autowired
+	private Datastore datastore;
 
-        //Then
-        final TestEntity result = datastore.get(TestEntity.class, save.getId());
-        assertThat(result).isNotNull();
-        assertThat(save.getId()).isEqualTo(result.getId());
-        assertThat(name).isEqualTo(result.getName());
-    }
+	@Test
+	public void shouldPersistEntity() {
+		try {
+			// Given
+			final String name = string().next();
+			final TestEntity testEntity = new TestEntity(null, name);
+
+			// When
+			final Key<TestEntity> save = datastore.save(testEntity);
+
+			// Then
+			final TestEntity result = datastore.get(TestEntity.class, save.getId());
+			assertThat(result).isNotNull();
+			assertThat(save.getId()).isEqualTo(result.getId());
+			assertThat(name).isEqualTo(result.getName());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
